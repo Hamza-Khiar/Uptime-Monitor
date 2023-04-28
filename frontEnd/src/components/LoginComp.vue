d746e1a25212f0fbd36d6f20c685da5eabbf3c0e
Hamza Khiar <hamza.khiar.01@gmail.com>
2023-04-28 07:07:40 +0100

    init Project

--- frontEnd/src/components/LoginComp.vue
+++ frontEnd/src/components/LoginComp.vue
@@ -6,5 +6,11 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <h1>Login Page</h1>
+  <form action="post">
+    <label for="email">Your Email</label>
+    <input type="text" name="email" id="" />
+    <label for="password">Your Password</label>
+    <input type="text" name="password" id="" />
+  </form>
   <RouterLink to="/register">don't have an account</RouterLink>
 </template>
--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content:[
-    './src/**/*.{js,tsx,vue,jsx}'
+    './src/**/*.{tsx,vue,jsx}',
+    './src/**/**/*.{tsx,vue,jsx}'
   ],
   theme: {
     extend: {},
